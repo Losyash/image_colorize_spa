890cfdd8954a9022aa8a44e6a4dfa133657ad75a
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-01-28 09:47:07 +0500

    Fixed code.

--- src/app.vue
+++ src/app.vue
@@ -6,45 +6,13 @@
       </div>
     </div>
 
-    <div class="card p-4 mb-4">
-      <div class="row">
-        <div class="col-12 d-flex justify-content-start pb-3">
-          <h5 class="section-caption">Выберите способ загрузки изображения</h5>
-        </div>
-
-        <div class="col-auto">
-          <u-radio v-model="dataType"
-            title="Загрузить из файла"
-            value="file"
-          />
-        </div>
-
-        <div class="col-auto">
-          <u-radio v-model="dataType"
-            title="Загрузить по ссылке"
-            value="link"
-          />
-        </div>
-      </div>
-    </div>
-    
-    <div class="card p-4">
+    <div class="card p-4 mb-3">
       <div class="row pb-4">
         <div class="col-12 d-flex justify-content-start pb-3">
-          <h5 class="section-caption">{{ dataType === 'link' ? 'Выберите ссылку' : 'Выберите файл' }}</h5>
+          <h5 class="section-caption">Выберите файл</h5>
         </div>
 
-        <div v-if="dataType === 'link'" class="col-12 d-flex justify-content-start pb-3">
-          <input v-model="link"
-            class="u-form-input"
-            placeholder="Вставьте ссылку на изображение"
-            type="text"
-          />
-        </div>
-
-        <div v-if="dataType === 'file'"
-          class="col-12 d-flex justify-content-start"
-        >
+        <div class="col-12 d-flex justify-content-start">
           <u-file :multiple="multiple"
             :mimes="mimes"
             @change="selectFile"
@@ -52,60 +20,103 @@
         </div>
       </div>
 
-      <div class="row d--flex justify-content-end pb-4">
-        <div class="col-auto">
-          <button style="color: #ffffff; background-color: #C0231F;"
+      <div v-if="inputImage"
+        class="row d--flex justify-content-end pb-4"
+      >
+        <div  class="col-auto">
+          <button class="u-button u-blue"
             @click="upload"
-          >Очистить</button>
-        </div>
-
-        <div class="col-auto">
-          <button style="color: #ffffff; background-color: #1E4391;"
-            @click="upload"
-          >Загрузить</button>
+          >Раскрасить изображение
+          </button>
         </div>
       </div>
-
-      <img :src="outputImage" alt="">
     </div>
+
+    <div v-if="inputImage || outputImage"
+      class="card p-4 mb-3"
+    >
+      <div class="row">
+        <div class="col-6">
+          <img :src="inputImage"
+            class="response-image"
+          />
+        </div>
+
+        <div class="col-6">
+          <img :src="outputImage"
+            class="response-image"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-if="inputImage || outputImage"
+      class="card p-4"
+    >
+      <div class="row d-flex justify-content-center">
+        <div class="col-12">
+          <USlider :afterImage="outputImage"
+            :beforeImage="inputImage"
+            :step="0.01"
+            :value="50"
+          />
+        </div>
+      </div>
+    </div>
+
+    <loading :active="isLoading" 
+      :can-cancel="false" 
+      :is-full-page="true"
+    />
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
+  import Loading from 'vue3-loading-overlay';
   import axios from 'axios';
   import utils from '@/libs/utils';
-  import URadio from '@/components/controls/URadio';
   import UFile from '@/components/controls/UFile';
+  import URadio from '@/components/controls/URadio';
+  import USlider from '@/components/USlider'
+  import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
 
   const mimes = ['image/png', 'image/jpeg'];
   const multiple = false;
 
-  const dataType = ref('file');
   const files = ref(null);
   const link = ref(null);
+  const isLoading = ref(false);
 
   const outputImage = ref(null);
+  const inputImage = ref(null);
 
-  const selectFile = (data) => files.value = data;
+  const selectFile = async (data) => {
+    outputImage.value = null;
+    inputImage.value = null;
+
+    files.value = data;
+
+    const uintdata = await utils.readFileAsArrayBuffer(files.value[0]);
+    const filedata = btoa(utils.arrayToString(uintdata))
+
+    inputImage.value = `data:image/jpeg;base64,${filedata}`
+  };
 
   const upload = async () => {
+    isLoading.value = true;
+
     try {
-      if (dataType.value === 'file') {
-        const uintdata = await utils.readFileAsArrayBuffer(files.value[0]);
-        const filedata = btoa(utils.arrayToString(uintdata))
+      const uintdata = await utils.readFileAsArrayBuffer(files.value[0]);
+      const filedata = btoa(utils.arrayToString(uintdata))
 
-        const { data } = await axios.post('/file', { file: filedata });
-        
-        outputImage.value = `data:image/jpeg;base64,${data}`
-      };
-
-      if (dataType.value === 'link') {
-        const { data } = await axios.post('/link', { link: link.value });
-      };
+      const { data } = await axios.post('/file', { file: filedata });
+      outputImage.value = `data:image/jpeg;base64,${data}`
     } catch (error) {
       console.log(error)
     };
+
+    isLoading.value = false;
   };
 </script>
 
