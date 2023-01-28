3669fec4f47318755e17a82f2d573ace78b46ca1
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-01-28 13:06:30 +0500

    Fix drop image.

--- src/components/controls/UFile.vue
+++ src/components/controls/UFile.vue
@@ -32,80 +32,68 @@
     }
   });
 
-  const emits = defineEmits([
-    'change'
-  ]);
-
+  const emits = defineEmits([ 'change' ]);
   const mimes = ref(props.mimes);
 
   const change = (e) => {
-          emits('change', [...e.target.files]);
-        }
-
+    emits('change', [...e.target.files]);
+  };
 
   const accept = computed(() => {
-          if (mimes && mimes.length) {
-            return mimes.join(',');
-          } else {
-            return null;
-          };
-        });
+    if (mimes && mimes.length) {
+      return mimes.join(',');
+    } else {
+      return null;
+    };
+  });
 
+  const drop = (e) => emits('change', e.dataTransfer.files);
+</script>
 
-        const drop = (e) => {
-          const files = [];
-  
-          [...e.dataTransfer.files].forEach((file) => {
-            if (mimes.includes(file.type)) {
-              files.push(file);
-            }
-          });
-  
-          emits('change', files);
-        }
-  </script>
-  
-  <style scoped>
-    .upload-area {
-      align-items: center;
-      border: 1px dashed #1E4391;
-      display: flex;
-      flex-wrap: wrap;
-      height: 80px;
-      justify-content: center;
-      max-width: 100%;
-      padding: 20px 10px;
-      position: relative;
-      transition: 0.2s;
-      width: 100%;
-      border-radius: 5px;
-    }
-    .upload-area:hover {
-      border: 1px dashed green;
-    }
+<style scoped>
+  .upload-area {
+    align-items: center;
+    border-radius: 5px;
+    border: 1px dashed #1E4391;
+    display: flex;
+    flex-wrap: wrap;
+    height: 80px;
+    justify-content: center;
+    max-width: 100%;
+    padding: 20px 10px;
+    position: relative;
+    transition: 0.2s;
+    width: 100%;
+  }
 
-    .upload-area:hover .upload-text {
-        color: green
-    }
+  .upload-area:hover {
+    border: 1px dashed green;
+  }
 
-    .upload-input {
-      cursor: pointer;
-      height: 100%;
-      left: 0;
-      opacity: 0;
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      width: 100%;
-    }
-    .upload-icon {
-      font-size: 30px;
-      color: #40a9ff;
-    }
-    .upload-text {
-      font-family: "Roboto Regular", sans-serif;
-      display: flex;
+  .upload-area:hover .upload-text {
+    color: green
+  }
+
+  .upload-input {
+    bottom: 0;
+    cursor: pointer;
+    height: 100%;
+    left: 0;
+    opacity: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100%;
+  }
+
+  .upload-icon {
+    color: #40a9ff;
+    font-size: 30px;
+  }
+
+  .upload-text {
+    display: flex;
+    font-family: "Roboto Regular", sans-serif;
     justify-content: center;
     width: 100%;
   }
